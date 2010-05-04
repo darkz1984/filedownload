d6883dd3e2557d34a7a434d3fe1ef25092145f89
emergingjosh <emergingjosh@d716b71f-1bda-9fda-5f91-60ace91f8fd0>
2010-05-04 14:10:33 +0000

    Added start of basic http authentication.

--- trunk/fileDownloader.py
+++ trunk/fileDownloader.py
@@ -1,6 +1,9 @@
 import os
-import sys
 import urllib2
+import sys
+import re
+import base64
+from urlparse import urlparse
 
 class DownloadFile(object):
 	"""Downloads files from http or ftp locations"""
@@ -63,6 +66,29 @@ class DownloadFile(object):
 		req.headers['Range'] = 'bytes=%s-%s' % (self.getLocalFileSize(), self.getUrlFileSize())
 		self.downloadFile(req)
 
+	def authHttp(self):
+		"""handles http basic authentication"""
+		passman = urllib2.HTTPPasswordMgrWithDefaultRealm()
+		# this creates a password manager
+		passman.add_password(None, theurl, username, password)
+		# because we have put None at the start it will always
+		# use this username/password combination for  urls
+		# for which `theurl` is a super-url
+		
+		authhandler = urllib2.HTTPBasicAuthHandler(passman)
+		# create the AuthHandler
+		
+		opener = urllib2.build_opener(authhandler)
+		
+		urllib2.install_opener(opener)
+		# All calls to urllib2.urlopen will now use our handler
+		# Make sure not to include the protocol in with the URL, or
+		# HTTPPasswordMgrWithDefaultRealm will be very confused.
+		# You must (of course) use it when fetching the page though.
+		
+		pagehandle = urllib2.urlopen(theurl)
+		# authentication is now handled automatically for us
+		print pagehandle.read()
 
 downloader = DownloadFile('http://download.thinkbroadband.com/200MB.zip')
 #downloader.downloadFile()
