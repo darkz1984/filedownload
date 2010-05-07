6f32db1d09efa40b507a939ed3ddc91e61453eb7
emergingjosh <emergingjosh@d716b71f-1bda-9fda-5f91-60ace91f8fd0>
2010-05-07 21:46:21 +0000

    Added comments and stringdocs. Changed some methods to private methods. Renamed a few methods. Added download and resume methods to make use clearer.

--- trunk/fileDownloader.py
+++ trunk/fileDownloader.py
@@ -1,3 +1,4 @@
+"""Downloads files from http or ftp locations"""
 import os
 import urllib2
 import sys
@@ -7,18 +8,43 @@ import urlparse
 import urllib
 
 class DownloadFile(object):
-	"""Downloads files from http or ftp locations"""
+	"""This class is used for downloading files from the internet via http or ftp.
+	If supports basic http authentication and ftp accounts, and supports resuming downloads. 
+	It does not support https or sftp.
+	
+	The main advantage of this class is it's ease of use, and pure pythoness, no C to compile.
+	
+	If a non-standard port is needed just include it in the url (http://example.com:7632).
+	
+	Basic usage:
+		Simple
+			downloader = fileDownloader.DownloadFile('http://example.com/file.zip')
+			downloader.download()
+	 	Use full path to download
+	 		downloader = fileDownloader.DownloadFile('http://example.com/file.zip', "C:\\Users\\bantonj\\Downloads\\newfilename.zip")
+	 		downloader.download()
+	 	Password protected download
+	 		downloader = fileDownloader.DownloadFile('http://example.com/file.zip', "C:\\Users\\bantonj\\Downloads\\newfilename.zip", ('jimmy','JHuyi5'))
+	 		downloader.download()
+	 	Resume
+	 		downloader = fileDownloader.DownloadFile('http://example.com/file.zip')
+			downloader.resume()
+	"""        
+	
 	def __init__(self, url, localFileName=None, auth=None):
+		"""Note that auth argument expects a tuple, ('username','password')"""
 		self.url = url
 		self.urlFileName = None
 		self.progress = None
 		self.localFileName = localFileName
 		self.type = self.getType()
 		self.auth = auth
+		#if no filename given pulls filename from the url
 		if not self.localFileName:
 			self.localFileName = self.getUrlFilename(self.url)
 		
-	def downloadFile(self, urlObj, fileObj):
+	def __downloadFile__(self, urlObj, fileObj):
+		"""starts the download loop"""
 		chunk = 1024
 		while 1:
 			data = urlObj.read(chunk)
@@ -29,78 +55,7 @@ class DownloadFile(object):
 			fileObj.write(data)
 			print "Read %s bytes"%len(data)
         
-	def getUrlFilename(self, url):
-		"""finds out the filename from an url"""
-		return urllib.unquote(os.path.basename(url))
-		
-	def getUrlFileSize(self):
-		"""gets filesize of remote file from ftp or http server"""
-		if self.type == 'http':
-			urllib2Obj = urllib2.urlopen(self.url)
-			return urllib2Obj.headers.get('content-length')
-		
-	def getLocalFileSize(self):
-		"""gets filesize of local file"""
-		size = os.stat(self.localFileName).st_size
-		return size
-		
-	def getType(self):
-		"""figures out if self.url is http or ftp"""
-		type = urlparse.urlparse(self.url).scheme
-		print type
-		return type
-		
-	def startHttpResume(self, restart=None):
-		"""starts to resume by getting the local filesize and calling downloadFile"""
-		if restart:
-			f = open(self.localFileName , "wb")
-		else:
-			f = open(self.localFileName , "ab")
-		if self.auth:
-			self.authHttp()
-		#to resume ftp probably have to subClass ftpHandler or just user ftplib
-		req = urllib2.Request(self.url)
-		req.headers['Range'] = 'bytes=%s-%s' % (self.getLocalFileSize(), self.getUrlFileSize())
-		urllib2Obj = urllib2.urlopen(req)
-		self.downloadFile(urllib2Obj, f)
-
-	def startFtpResume(self, restart=None):
-		"""starts to resume"""
-		ftper = ftplib.FTP()
-		parseObj = urlparse.urlparse(self.url)
-		baseUrl= parseObj.hostname
-		urlPort = parseObj.port
-		bPath = os.path.basename(parseObj.path)
-		gPath = parseObj.path.replace(bPath, "")
-		unEncgPath = urllib.unquote(gPath)
-		fileName = urllib.unquote(os.path.basename(self.url))
-		ftper.connect(baseUrl, urlPort)
-		ftper.login(self.auth[0], self.auth[1])
-		print gPath
-		if len(gPath) > 1:
-			ftper.cwd(unEncgPath)
-		ftper.sendcmd("TYPE I")
-		ftper.sendcmd("REST " + str(self.getLocalFileSize()))
-		downCmd = "RETR "+ fileName
-		print downCmd
-		ftper.retrbinary(downCmd, open(fileName, 'ab').write)
-		
-
-	def startNormal(self):
-		"""starts the file download normally"""
-		f = open(self.localFileName , "wb")
-		if self.auth:
-			if self.type == 'http':
-				authObj = self.authHttp()
-				self.downloadFile(authObj, f)
-			elif self.type == 'ftp':
-				authObj = self.authFtp()
-				self.downloadFile(authObj, f)
-		else:
-			urllib2Obj = urllib2.urlopen(self.url)
-			self.downloadFile(urllib2Obj, f)
-
-	def authHttp(self):
+	def __authHttp__(self):
 		"""handles http basic authentication"""
 		passman = urllib2.HTTPPasswordMgrWithDefaultRealm()
 		# this creates a password manager
@@ -125,7 +80,7 @@ class DownloadFile(object):
 		#print pagehandle.read()
 		return pagehandle
 		
-	def authFtp(self):
+	def __authFtp__(self):
 		"""handles ftp authentication"""
 		ftped = urllib2.FTPHandler()
 		ftpUrl = self.url.replace('ftp://', '')
@@ -134,8 +89,80 @@ class DownloadFile(object):
 		req.timeout = 120
 		ftpObj = ftped.ftp_open(req)
 		return ftpObj
+        
+	def __startHttpResume__(self, restart=None):
+		"""starts to resume HTTP"""
+		if restart:
+			f = open(self.localFileName , "wb")
+		else:
+			f = open(self.localFileName , "ab")
+		if self.auth:
+			self.__authHttp__()
+		#to resume ftp probably have to subClass ftpHandler or just user ftplib
+		req = urllib2.Request(self.url)
+		req.headers['Range'] = 'bytes=%s-%s' % (self.getLocalFileSize(), self.getUrlFileSize())
+		urllib2Obj = urllib2.urlopen(req)
+		self.__downloadFile__(urllib2Obj, f)
 
-downloader = DownloadFile('ftp://files.emergingpictures.com/OPERA%20and%20BALLET/OPERA/l%27Orfeo/TRAILER/Orfeo%20Live%20Trailer%20720p%20V2.mov', auth=('fullaccess', 'Emerging245'))
-# downloader.startNormal()
-downloader.startFtpResume()
-#urlretrieve("ftp://ftp.gimp.org/pub/gimp/v2.6/patch-2.6.5.bz2")
+	def __startFtpResume__(self, restart=None):
+		"""starts to resume FTP"""
+		ftper = ftplib.FTP()
+		parseObj = urlparse.urlparse(self.url)
+		baseUrl= parseObj.hostname
+		urlPort = parseObj.port
+		bPath = os.path.basename(parseObj.path)
+		gPath = parseObj.path.replace(bPath, "")
+		unEncgPath = urllib.unquote(gPath)
+		fileName = urllib.unquote(os.path.basename(self.url))
+		ftper.connect(baseUrl, urlPort)
+		ftper.login(self.auth[0], self.auth[1])
+		print gPath
+		if len(gPath) > 1:
+			ftper.cwd(unEncgPath)
+		ftper.sendcmd("TYPE I")
+		ftper.sendcmd("REST " + str(self.getLocalFileSize()))
+		downCmd = "RETR "+ fileName
+		print downCmd
+		ftper.retrbinary(downCmd, open(fileName, 'ab').write)
+        
+	def getUrlFilename(self, url):
+		"""returns filename from url"""
+		return urllib.unquote(os.path.basename(url))
+		
+	def getUrlFileSize(self):
+		"""gets filesize of remote file from ftp or http server"""
+		if self.type == 'http':
+			urllib2Obj = urllib2.urlopen(self.url)
+			return urllib2Obj.headers.get('content-length')
+		
+	def getLocalFileSize(self):
+		"""gets filesize of local file"""
+		size = os.stat(self.localFileName).st_size
+		return size
+		
+	def getType(self):
+		"""returns protocol of url (ftp or http)"""
+		type = urlparse.urlparse(self.url).scheme
+		return type	
+
+	def download(self):
+		"""starts the file download"""
+		f = open(self.localFileName , "wb")
+		if self.auth:
+			if self.type == 'http':
+				authObj = self.__authHttp__()
+				self.__downloadFile__(authObj, f)
+			elif self.type == 'ftp':
+				authObj = self.__authFtp__()
+				self.__downloadFile__(authObj, f)
+		else:
+			urllib2Obj = urllib2.urlopen(self.url)
+			self.__downloadFile__(urllib2Obj, f)
+
+	def resume(self):
+		"""attempts to resume file download"""
+		type = self.getType()
+		if type == 'http':
+			self.startHttpResume()
+		elif type == 'ftp':
+			self.startFtpResume()
